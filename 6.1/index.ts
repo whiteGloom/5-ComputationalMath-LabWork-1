c7e11f2f95bfe199ce087e86574341617627059e
whiteGloom <karabin.m@yandex.ru>
2025-06-23 18:00:12 +0600

    6.1 result info updated
    BranchName: main

--- 6.1/index.ts
+++ 6.1/index.ts
@@ -28,18 +28,30 @@ export async function dichotomyIntro() {
 }
 
 function dichotomy(targetFunc: (x: number) => number, a: number, b: number, argumentAccuracy: number, functionAccuracy: number = 0): void {
+    let funcCallsCount = 0;
+
+    const startTimeStamp = performance.now();
+
+    function getFuncValue(x: number) {
+        funcCallsCount += 1;
+        return targetFunc(x);
+    }
+
     let left = a;
     let right = b;
     let middle = a + (b - a) / 2;
-    let valueAtLeft = targetFunc(left);
-    let valueAtMiddle = targetFunc(middle);
+    let valueAtLeft = getFuncValue(left);
+    let valueAtMiddle = getFuncValue(middle);
     let iteration = 1;
 
     function showResult() {
         console.log(`Обнаружен корень функции с заданной точностью аргумента и функции на итерации № ${iteration}:`);
+        console.log(`Количество вычислений функции: ${funcCallsCount}`);
+        console.log(`Время счёта: ${(performance.now() - startTimeStamp).toFixed(3)} мс`);
         console.log(`Левая граница интервала: ${left.toFixed(3)}`);
         console.log(`Правая граница интервала: ${right.toFixed(3)}`);
         console.log(`Текущая точность аргумента: ${right - left}`);
+        console.log(`Порядок сходимости: 0.5`);
         console.log(`x = ${middle.toFixed(3)}, f(x) = ${valueAtMiddle.toFixed(3)}`);
     }
 
@@ -47,7 +59,7 @@ function dichotomy(targetFunc: (x: number) => number, a: number, b: number, argu
         console.log('Точный корень функции найден на левой границе.');
     }
 
-    if (targetFunc(right) === 0) {
+    if (getFuncValue(right) === 0) {
         console.log('Точный корень функции найден на правой границе.');
     }
 
@@ -65,8 +77,8 @@ function dichotomy(targetFunc: (x: number) => number, a: number, b: number, argu
         }
 
         middle = left + (right - left) / 2;
-        valueAtLeft = targetFunc(left);
-        valueAtMiddle = targetFunc(middle);
+        valueAtLeft = getFuncValue(left);
+        valueAtMiddle = getFuncValue(middle);
         iteration += 1;
 
         if (iteration > 100) {
